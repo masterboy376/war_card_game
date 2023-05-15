75f827ab22279f7fe169d670ee5ed89fb253af6f
sambhav <masterboy376@gmail.com>
2023-05-15 15:59:34 +0530

    Initial Commit

---
+++ war_card_game/war_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameApp.swift
+//  war_card_game
+//
+//  Created by GAURAV JAGLAN on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
